32f0b9675f748c99e035256c3381cc25ac91a298
overdrivenpotato <marko.mijalkovic97@gmail.com>
2014-05-02 12:56:11 -0400

    Added custom exception class

---
+++ Main/src/com/github/itsaunixsystem/chunks/ChunksException.java
@@ -0,0 +1,11 @@
+package com.github.itsaunixsystem.chunks;
+
+public class ChunksException extends Exception {
+    public ChunksException(String message) {
+        super(message);
+    }
+
+    public ChunksException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
