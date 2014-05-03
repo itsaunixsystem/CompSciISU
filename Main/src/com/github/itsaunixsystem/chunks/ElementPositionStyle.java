7b516d486b6ec8c91e01a21dea7160f8a89baa99
overdrivenpotato <marko.mijalkovic97@gmail.com>
2014-05-03 17:36:27 -0400

    Added ElementPositionStyle enumerator. This lets you center elements/use offsets for positioning.

---
+++ Main/src/com/github/itsaunixsystem/chunks/ElementPositionStyle.java
@@ -0,0 +1,31 @@
+package com.github.itsaunixsystem.chunks;
+
+import java.util.function.IntUnaryOperator;
+
+@SuppressWarnings(value = "unused")
+public enum ElementPositionStyle {
+    FIXED(screenWidth -> 0, screenHeight -> 0),
+
+    H_CENTER(screenWidth -> screenWidth / 2, screenHeight -> 0),
+    V_CENTER(screenWidth -> 0, screenHeight -> screenHeight / 2),
+    VH_CENTER(screenWidth -> screenWidth / 2, screenHeight -> screenHeight / 2),
+
+    TOP_OFFSET(screenWidth -> 0, screenHeight -> screenHeight),
+    RIGHT_OFFSET(screenWidth -> screenWidth, screenHeight -> 0),
+    TOP_RIGHT_OFFSET(screenWidth -> screenWidth, screenHeight -> screenHeight);
+
+    private IntUnaryOperator x, y;
+
+    private ElementPositionStyle(IntUnaryOperator x, IntUnaryOperator y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getXOffset(int screenWidth) {
+        return x.applyAsInt(screenWidth);
+    }
+
+    public int getYOffset(int screenHeight) {
+        return y.applyAsInt(screenHeight);
+    }
+}
