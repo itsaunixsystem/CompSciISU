a79f29886fe518c613ad16013e704597b5ec1c3f
overdrivenpotato <marko.mijalkovic97@gmail.com>
2014-05-03 16:38:43 -0400

    Removed useless unimplemented methods

--- Main/src/com/github/itsaunixsystem/chunks/ScreenMainMenu.java
+++ Main/src/com/github/itsaunixsystem/chunks/ScreenMainMenu.java
@@ -72,41 +72,4 @@ public class ScreenMainMenu extends GuiScreen {
     public boolean scrolled(int amount) {
         return false;
     }
-
-    //-------------GUI---------------
-//
-//    @Override
-//    public boolean keyDown(int keycode) {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean keyUp(int keycode) {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean keyTyped(char character) {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean touchDragged(int screenX, int screenY, int pointer) {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean mouseMoved(int screenX, int screenY) {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean scrolled(int amount) {
-//        return false;
-//    }
 }
