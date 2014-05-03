8a459b71f82d3b25bd61b56e1c7e2c33aed26898
overdrivenpotato <marko.mijalkovic97@gmail.com>
2014-05-03 18:26:31 -0400

    Implemented ElementPositionStyle to GuiBase

--- Main/src/com/github/itsaunixsystem/chunks/GuiBase.java
+++ Main/src/com/github/itsaunixsystem/chunks/GuiBase.java
@@ -1,18 +1,34 @@
 package com.github.itsaunixsystem.chunks;
 
+import com.badlogic.gdx.Gdx;
+
 import java.awt.geom.Rectangle2D;
 
 public abstract class GuiBase implements GuiElement {
     protected Rectangle2D.Float outline;
     protected GuiRenderer renderer;
+    protected ElementPositionStyle posStyle;
 
     public GuiBase(Rectangle2D.Float outline) {
         this.outline = outline;
         renderer = new GuiRenderer();
     }
 
+    public GuiBase(Rectangle2D.Float outline, ElementPositionStyle posStyle) {
+        this(outline);
+        this.posStyle = posStyle;
+    }
+
+    protected float getX() {
+        return (float) (outline.x + posStyle.getXOffset(Gdx.graphics.getWidth()) - (outline.getWidth() / 2));
+    }
+
+    protected float getY() {
+        return (float) (outline.y + posStyle.getYOffset(Gdx.graphics.getHeight()) - (outline.getHeight() / 2));
+    }
+
     public boolean withinBounds(int x, int y) {
-        return outline.contains(x, y);
+        return new Rectangle2D.Float(getX(), getY(), outline.width, outline.height).contains(x, y);
     }
 
     public void refreshRenderer() {
--- Main/src/com/github/itsaunixsystem/chunks/GuiButton.java
+++ Main/src/com/github/itsaunixsystem/chunks/GuiButton.java
@@ -13,7 +13,11 @@ public class GuiButton extends GuiBase implements GuiClickable {
     protected String text;
 
     public GuiButton(String text, Rectangle2D.Float outline, GuiClickable onClick) {
-        super(outline);
+        this(text, outline, ElementPositionStyle.FIXED, onClick);
+    }
+
+    public GuiButton(String text, Rectangle2D.Float outline, ElementPositionStyle posStyle, GuiClickable onClick) {
+        super(outline, posStyle);
         this.onClick = onClick;
         pointerHovering = new boolean[20]; //Max 20 fingers or game breaks
         this.text = text;
@@ -23,9 +27,11 @@ public class GuiButton extends GuiBase implements GuiClickable {
     public void render(float delta) {
         ShapeRenderer shapeRenderer = this.renderer.getShapeRenderer();
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
         if(hovering()) {
             shapeRenderer.setColor(0.6f, 0.6f, 0.6f, 0.1f);
-            shapeRenderer.rect(outline.x, outline.y, (float) outline.getWidth(), (float) outline.getHeight());
+            shapeRenderer.rect(getX(), getY(), (float) outline.getWidth(), (float) outline.getHeight());
+
             if(Gdx.input.isButtonPressed(0) ||
                     Gdx.app.getType() == Application.ApplicationType.Android ||
                     Gdx.app.getType() == Application.ApplicationType.iOS) {
@@ -33,20 +39,21 @@ public class GuiButton extends GuiBase implements GuiClickable {
             } else {
                 shapeRenderer.setColor(0.8f, 0.8f, 0.8f, 0.5f);
             }
-            shapeRenderer.rect(outline.x + 2, outline.y + 2, (float) outline.getWidth() - 4, (float) outline.getHeight() - 4);
+
+            shapeRenderer.rect(getX() + 2, getY() + 2, (float) outline.getWidth() - 4, (float) outline.getHeight() - 4);
         } else {
             shapeRenderer.setColor(0.5f, 0.5f, 0.5f, 0.9f);
-            shapeRenderer.rect(outline.x, outline.y, (float) outline.getWidth(), (float) outline.getHeight());
+            shapeRenderer.rect(getX(), getY(), (float) outline.getWidth(), (float) outline.getHeight());
             shapeRenderer.setColor(0.6f, 0.6f, 0.6f, 0.7f);
-            shapeRenderer.rect(outline.x + 2, outline.y + 2, (float) outline.getWidth() - 4, (float) outline.getHeight() - 4);
+            shapeRenderer.rect(getX() + 2, getY() + 2, (float) outline.getWidth() - 4, (float) outline.getHeight() - 4);
         }
         shapeRenderer.end();
 
         SpriteBatch spriteBatch = renderer.getSpriteBatch();
         spriteBatch.begin();
         renderer.getFontRenderer().draw(spriteBatch, text,
-                (float) (outline.x + outline.getWidth() / 2 - renderer.getFontRenderer().getSpaceWidth() * text.length() / 2),
-                (float) (outline.y + outline.getHeight() / 2 + renderer.getFontRenderer().getCapHeight() / 2));
+                (float) (getX() + outline.getWidth() / 2 - renderer.getFontRenderer().getSpaceWidth() * text.length() / 2),
+                (float) (getY() + outline.getHeight() / 2 + renderer.getFontRenderer().getCapHeight() / 2));
         spriteBatch.end();
     }
 
