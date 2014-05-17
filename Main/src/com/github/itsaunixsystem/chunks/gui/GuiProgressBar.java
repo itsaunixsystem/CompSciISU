88e6a76f6bb226e75a6939f6d9b0fb4584e73c03
overdrivenpotato <marko.mijalkovic97@gmail.com>
2014-05-17 18:55:15 -0400

    Made GuiProgressBar look nicer

--- Main/src/com/github/itsaunixsystem/chunks/gui/GuiProgressBar.java
+++ Main/src/com/github/itsaunixsystem/chunks/gui/GuiProgressBar.java
@@ -6,7 +6,10 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import java.awt.geom.Rectangle2D;
 
 public class GuiProgressBar extends GuiBase {
-    private float progress = 0;
+    private float concreteProgress = 0;
+    private float travelDistance;
+    private float startProgress;
+    private float lastLoaded = 0f;
 
     public GuiProgressBar(Rectangle2D.Float outline) {
         super(outline);
@@ -18,6 +21,12 @@ public class GuiProgressBar extends GuiBase {
 
     @Override
     protected void draw(float delta) {
+
+        travelDistance += delta * 40f;
+        if(travelDistance + startProgress > concreteProgress) {
+            travelDistance = concreteProgress - startProgress;
+        }
+        float endRect = travelDistance + startProgress;
         ShapeRenderer shapeRenderer = renderer.getShapeRenderer();
 
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
@@ -26,26 +35,31 @@ public class GuiProgressBar extends GuiBase {
         shapeRenderer.rect(getX(), getY(), outline.width, outline.height);
 
         shapeRenderer.setColor(0.1f, 0.7f, 0.1f, 0.9f);
-        shapeRenderer.rect(getX() + 3, getY() + 3, (outline.width - 6) * (progress / 100.0f), outline.height - 6);
+        shapeRenderer.rect(getX() + 3, getY() + 3, (outline.width - 6) * (endRect / 100.0f), outline.height - 6);
 
-        Color solid = new Color(1f, 1f, 1f, 0.4f * (progress / 100.f));
+        Color solid = new Color(1f, 1f, 1f, 0.4f * (endRect / 100.f));
         Color clear = new Color(0, 0, 0, 0);
-        shapeRenderer.rect(getX() + 3, getY() + 3, (outline.width - 6) * (progress / 100.0f), outline.height - 6,
+        shapeRenderer.rect(getX() + 3, getY() + 3, (outline.width - 6) * (endRect / 100.0f), outline.height - 6,
                 solid, clear, clear, solid);
 
         if(hovering()) {
             shapeRenderer.setColor(1f, 1f, 1f, 0.05f);
-            shapeRenderer.rect(getX() + 3, getY() + 3, (outline.width - 6) * (progress / 100.0f), outline.height - 6);
+            shapeRenderer.rect(getX() + 3, getY() + 3, (outline.width - 6) * (endRect / 100.0f), outline.height - 6);
         }
 
         shapeRenderer.end();
     }
 
     public void updateProgress(float progress) {
-        this.progress = progress;
+        if(lastLoaded != progress) {
+            this.startProgress = this.concreteProgress;
+            this.concreteProgress = progress;
+            lastLoaded = progress;
+            travelDistance = 0;
+        }
     }
 
     public void addProgress(float progress) {
-        this.progress += progress;
+        this.concreteProgress += progress;
     }
 }
